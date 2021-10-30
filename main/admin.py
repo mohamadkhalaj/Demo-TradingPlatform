040808e2f20023e86f78e3d1cacc42210c24e0bb
Amir Mousavi <s.amirhosein.mn@gmail.com>
2021-10-30 16:08:08 +0330

    new files has been added

---
+++ main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import UsrRegistration,TradeHistory,Portfolio
+
+admin.site.register(UsrRegistration)
+admin.site.register(TradeHistory)
+admin.site.register(Portfolio)
