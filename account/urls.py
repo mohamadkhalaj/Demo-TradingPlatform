722c26e8e5e88243a33702a49b1e6a2872200bc2
Amir Mousavi <s.amirhosein.mn@gmail.com>
2021-11-08 13:33:22 +0330

    change and reset password urls created.

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,8 @@ from .views import wallet, settings, profile, trade
 app_name = 'account'
 urlpatterns = [
 	path('login/', views.LoginView.as_view(), name='login'),
+	path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 	path('wallet/', wallet, name='wallet'),
 	path('settings/', settings, name='settings'),
 	path('profile/', profile, name='profile'),
@@ -15,8 +17,8 @@ urlpatterns = [
 # urlpatterns = [
 #     path('logout/', views.LogoutView.as_view(), name='logout'),
 
-#     path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-#     path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
 #     path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
 #     path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
