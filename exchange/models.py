cc48e602fd306aa0642d9939d59b0fca136bb63b
unknown <you@example.com>
2021-11-10 18:23:42 +0330

    few changes in database

--- exchange/models.py
+++ exchange/models.py
@@ -3,20 +3,18 @@ from django.db import models
 
 class TradeHistory(models.Model):
 	id = models.AutoField(primary_key=True)
-	type = models.CharField(max_length=1)  # s/b
-	cryptoName = models.CharField(max_length=10, default=None)
-	price = models.CharField(max_length=15)
-	date = models.DateField(auto_now=True)
-	amount = models.CharField(max_length=15, default='0')
-	equivalentAmount = models.CharField(max_length=15, default='0')
+	type = models.CharField(max_length=4)
+	pair = models.CharField(max_length=10, default=None)
+	pairPrice = models.FloatField()
+	amount = models.FloatField(default=0)
+	price = models.FloatField()
+	timeStamp = models.FloatField()
 
 
 class Portfolio(models.Model):
-	id = models.AutoField(primary_key=True)
-	cryptoName = models.CharField(max_length=10, default=None)
-	price = models.CharField(max_length=15)
-	date = models.DateField(auto_now=True)
-	amount = models.CharField(max_length=15, default='0')
-	equivalentAmount = models.CharField(max_length=15, default='0')
+	cryptoName = models.CharField(max_length=10, default=None, primary_key=True)
+	amount = models.FloatField(default=0, null=True)
+	equivalentAmount = models.FloatField(default=0, null=True)
+
 
 
