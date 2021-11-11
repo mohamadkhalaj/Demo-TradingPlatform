b7f0630f06f1261f22f3a459cf41b9e3068cd563
Mohammad Khalaj <mohammadkhalaj8@gmail.com>
2021-11-11 15:11:16 +0100

    profile template eddited and login with google btn added.

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Portfolio, TradeHistory
 
-# Register your models here.
+class TradeHistoryAdmin(admin.ModelAdmin):
+	list_display = ('pair', 'pairPrice', 'amount', 'price', 'timeStamp')
+
+admin.site.register(TradeHistory, TradeHistoryAdmin)
+
+class PortfolioAdmin(admin.ModelAdmin):
+	list_display = ('cryptoName', 'amount', 'equivalentAmount')
+
+admin.site.register(Portfolio, PortfolioAdmin)
