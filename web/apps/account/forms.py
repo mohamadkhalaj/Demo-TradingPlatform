732341aa1aaf8b320879785a1f5a3b12bc482a04
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-07-31 20:41:01 +0430

    Fix hcaptcha bug

--- web/apps/account/forms.py
+++ web/apps/account/forms.py
@@ -48,4 +48,4 @@ class SignupForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2", "hcaptcha")
+        fields = ("username", "email", "password1", "password2")
