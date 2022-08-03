e077bd3e07b20c73433f6a58e465da9f3e4d9b25
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-08-03 16:38:29 +0430

    Move account related urls from (/) to (/account)

--- web/config/urls.py
+++ web/config/urls.py
@@ -9,8 +9,8 @@ from .views import home
 app_name = "config"
 urlpatterns = [
     path("", home, name="home"),
-    path("login/", Login.as_view(), name="login"),
-    path("signup/", Register.as_view(), name="signUp"),
+    path("account/login/", Login.as_view(), name="login"),
+    path("account/signup/", Register.as_view(), name="signUp"),
     path("password_reset/", PasswordReset.as_view(), name="password_reset"),
     path("password_change/", PasswordChange.as_view(), name="password_change"),
     path("activate/<uidb64>/<token>/", activate, name="activate"),
@@ -18,7 +18,7 @@ urlpatterns = [
     path("admin/", include("admin_honeypot.urls", namespace="admin_honeypot")),
     path("", include("exchange.urls")),
     path("account/", include("account.urls")),
-    path("", include("django.contrib.auth.urls")),
+    path("account/", include("django.contrib.auth.urls")),
     path("", include("social_django.urls", namespace="social")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
