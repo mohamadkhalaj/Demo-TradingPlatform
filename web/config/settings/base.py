c55cf652162ddd272b3aa5e3008b05ab5e9e91d5
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-08-03 17:59:13 +0430

    Remove updateUserDetail

--- web/config/settings/base.py
+++ web/config/settings/base.py
@@ -213,7 +213,6 @@ SOCIAL_AUTH_PIPELINE = (
     "social_core.pipeline.user.create_user",
     "social_core.pipeline.social_auth.associate_user",
     "social_core.pipeline.social_auth.load_extra_data",
-    "social_core.pipeline.user.user_details",
 )
 SOCIAL_AUTH_PROTECTED_USER_FIELDS = [
     "email",
