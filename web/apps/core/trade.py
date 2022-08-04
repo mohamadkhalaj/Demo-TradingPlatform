26a2c9b04113ca3e3e5cc8be6be5bac73800076b
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-08-04 14:36:58 +0430

    Remove matplotlib

--- web/apps/core/trade.py
+++ web/apps/core/trade.py
@@ -1,51 +1,52 @@
 from django.conf import settings
-from matplotlib.pyplot import hist
-from .utils import calc_equivalent
 from exchange.models import Portfolio, TradeHistory
+
+from .utils import calc_equivalent
 
 
 class Trade:
     def __init__(self, user, inputs):
         self.user = user
-        self.portfo = Portfolio.objects.filter(usr=self.user, marketType='spot')
-        self.orderType = inputs['orderType']
-        self.type = inputs['type']
-        self.pair = inputs['pair']
-        self.base = inputs['pair'].split('-')[0]
-        self.quote = inputs['pair'].split('-')[1]
-        self.crp = inputs['amount'].split(' ')[1]
-        self.amount = float(inputs['amount'].split(' ')[0])
+        self.portfo = Portfolio.objects.filter(usr=self.user, marketType="spot")
+        self.orderType = inputs["orderType"]
+        self.type = inputs["type"]
+        self.pair = inputs["pair"]
+        self.base = inputs["pair"].split("-")[0]
+        self.quote = inputs["pair"].split("-")[1]
+        self.crp = inputs["amount"].split(" ")[1]
+        self.amount = float(inputs["amount"].split(" ")[0])
 
-    
     def spotTrade(self):
         message = None
         pairPrice = calc_equivalent(self.base, self.quote)
 
         if self.crp == self.base:
             equivalent = self.amount * pairPrice
-            amount = f'{self.amount} {self.base}'
+            amount = f"{self.amount} {self.base}"
 
             if equivalent < settings.MINIMUM_TRADE_SIZE:
-                message = f"minimum trade size is {settings.MINIMUM_TRADE_SIZE} $, but your's is: "\
-                            +f"{self.amount} {self.crp}={round(equivalent, 2)}$ !"
+                message = (
+                    f"minimum trade size is {settings.MINIMUM_TRADE_SIZE} $, but your's is: "
+                    + f"{self.amount} {self.crp}={round(equivalent, 2)}$ !"
+                )
             else:
-                if self.type == 'buy':
+                if self.type == "buy":
                     is_available = self.check_available(equivalent, self.quote)
                     toAdd = [self.base, self.amount]
                     toSub = [self.quote, equivalent]
                 else:
                     is_available = self.check_available(self.amount, self.base)
                     toAdd = [self.quote, equivalent]
-                    toSub = [self.base, self.amount]  
+                    toSub = [self.base, self.amount]
 
         else:
             equivalent = self.amount / pairPrice
-            amount = f'{equivalent} {self.base}'
+            amount = f"{equivalent} {self.base}"
 
             if self.amount < settings.MINIMUM_TRADE_SIZE:
                 message = f"minimum trade size is {settings.MINIMUM_TRADE_SIZE} $, but your's is: {self.amount} $ !"
             else:
-                if self.type == 'buy':
+                if self.type == "buy":
                     is_available = self.check_available(self.amount, self.quote)
                     toAdd = [self.base, equivalent]
                     toSub = [self.quote, self.amount]
@@ -55,10 +56,7 @@ class Trade:
                     toSub = [self.base, equivalent]
 
         if message:
-            tradeResponse = {
-                    "successful": False,
-                    "message": message
-            }
+            tradeResponse = {"successful": False, "message": message}
             return tradeResponse
 
         if is_available:
@@ -85,13 +83,10 @@ class Trade:
                 amount=amount,
                 complete=True,
                 orderType=self.orderType,
-                pairPrice=pairPrice
+                pairPrice=pairPrice,
             )
 
-            tradeResponse = {
-                "successful": True,
-                "message": 'Order filled!'
-            }
+            tradeResponse = {"successful": True, "message": "Order filled!"}
 
             tradeResult = {
                 "0": {
@@ -107,13 +102,9 @@ class Trade:
             return tradeResponse, tradeResult, executed_time
 
         else:
-            tradeResponse = {
-                "successful": False,
-                "message": 'Insufficient balance!'
-            }
+            tradeResponse = {"successful": False, "message": "Insufficient balance!"}
 
         return tradeResponse
-
 
     def check_available(self, amount, name):
         try:
@@ -123,4 +114,4 @@ class Trade:
             else:
                 return False
         except:
-            return False   
+            return False
