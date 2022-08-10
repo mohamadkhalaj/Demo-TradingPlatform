cb31fab6aa9cdf1b2230c5fcc08bebeb592340f0
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-08-10 08:50:11 +0430

    Fix admin page bugs

--- web/apps/middlewares/visitors.py
+++ web/apps/middlewares/visitors.py
@@ -1,26 +1,27 @@
+from os import environ as env
+
+from core.utils import get_user_ip
+
 from .models import visitor
 
 
 def vistorsMiddleware(get_response):
     def middleware(request):
-        IGNORED_PATH = [
-            "/admin/jsi18n/",
-            "/admin/api/visitor/",
+        ADMIN_URL = env.get("DJANGO_ADMIN_URL", "admin").replace("/", "")  # Get admin url
+        IGNORED_PATH = [  # Ignore this paths for logging
+            f"/{ADMIN_URL}/jsi18n/",
+            f"/{ADMIN_URL}/middlewares/visitor/",
             "/favicon.ico",
         ]
 
-        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(",")[0]
-        else:
-            ip = request.META.get("REMOTE_ADDR")
+        ip = get_user_ip(request)
 
         userAgent = request.META.get("HTTP_USER_AGENT")
         path = request.META.get("PATH_INFO")
 
         if path not in IGNORED_PATH:
             obj = visitor(ip_address=ip, userAgent=userAgent, path=path)
-            if path.startswith("/admin/"):
+            if path.startswith(f"/{ADMIN_URL}/") and request.user.is_authenticated:
                 obj.isAdminPanel = True
             obj.save()
 
